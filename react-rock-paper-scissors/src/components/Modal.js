f4e18bd4429b09e33472ea10fe310ee321030233
spencerrunde <spencerrunde@gmail.com>
2022-02-03 14:55:01 -0800

    Added functionality (close modal on click outside)

--- react-rock-paper-scissors/src/components/Modal.js
+++ react-rock-paper-scissors/src/components/Modal.js
@@ -1,11 +1,29 @@
-import { React } from "react";
+import { React, useEffect, useRef } from "react";
+
 import ReactDOM from "react-dom";
 
 // rendering modal using createPortal and a separate root element
 function Modal({ toggleModal }) {
+  const wrapperRef = useRef(null);
+
+  useEffect(() => {
+    document.addEventListener("click", handleClickOutside, false);
+    return () => {
+      document.removeEventListener("click", handleClickOutside, false);
+    };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  // fires toggleModal function if you click backdrop of modal
+  const handleClickOutside = (event) => {
+    if (wrapperRef.current && !wrapperRef.current.contains(event.target)) {
+      toggleModal();
+    }
+  };
+
   return ReactDOM.createPortal(
     <div className="container">
-      <div className="container__wrapper">
+      <div className="container__wrapper" ref={wrapperRef}>
         <h1>Rules</h1>
         <div className="container__rules">
           <img src="images/image-rules-bonus.svg" alt="rules diagram" />
